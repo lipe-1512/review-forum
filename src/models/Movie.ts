878683e5e11b67afa438848fe9a3bfcd8c321747
lipe <felipelolgxx@gmail.com>
2025-07-17 06:23:34 -0300

    feat: add releaseYear and genre to Movie model

--- src/models/Movie.ts
+++ src/models/Movie.ts
@@ -1,20 +1,27 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn} from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, OneToMany } from 'typeorm';
+import { Review } from './Review';
+import { UserListItem } from './UserListItem';
 
 @Entity()
 export class Movie {
 
     @PrimaryGeneratedColumn()
-    id!: Number
+    id!: Number;
 
     @Column()
-    name: string
+    name: string;
 
     @Column()
-    description!: string
+    description!: string;
 
     @CreateDateColumn()
-    created_at!: Date
+    created_at!: Date;
 
+    @OneToMany(() => Review, review => review.movie)
+    reviews!: Review[];
+
+    @OneToMany(() => UserListItem, userListItem => userListItem.movie)
+    listItems!: UserListItem[];
 
   constructor(name: string, description: string) {
     this.name = name;
