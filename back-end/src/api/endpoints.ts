6b1d3ebe7dee06d56fe771f85a7fb5a525354e39
lipe <felipelolgxx@gmail.com>
2025-08-07 09:40:17 -0300

    feat: add User, Review and UserListItem models and repositories

--- back-end/src/api/endpoints.ts
+++ back-end/src/api/endpoints.ts
@@ -2,11 +2,19 @@ import commentRouter from "./CommentApi";
 import forumRouter from "./ForumApi";
 import MovieRouter from "./MovieApi";
 import { Router } from "express";
+import userRoutes from "./UserApi";
+import reviewRoutes from "./ReviewApi";
+import userListRoutes from "./UserListApi";
+import notificationRoutes from "./NotificationApi";
 
 const endpointsRouter = Router();   
 
 endpointsRouter.use('/movies', MovieRouter)
 endpointsRouter.use('/forums', forumRouter)
 endpointsRouter.use('/comments', commentRouter)
+endpointsRouter.use('/users', userRoutes)
+endpointsRouter.use('/reviews', reviewRoutes)
+endpointsRouter.use('/lists', userListRoutes)
+endpointsRouter.use('/notifications', notificationRoutes)
 
 export default endpointsRouter
